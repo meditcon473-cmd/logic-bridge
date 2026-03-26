6391bcce6647f2aab7e68f32118987efaac705ff
mac <mac@macdeMac-mini.local>
2026-03-26 11:46:29 +0800

    fix: 移除 node_modules 修复 Vercel 构建报错

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graph_1 = require("./graph");
-// 测试入口
-const testPrompt = "请以'青年与时代'为题写一篇议论文";
-(0, graph_1.runZhiyin)(testPrompt).catch(console.error);
